53d54293075cc21ee1f1a0e3bda8bc4f4792feab
Gabe-Lee <gla.ander@gmail.com>
2019-12-18 14:09:16 -0600

    Restructure repo and eliminate tech debt

---
+++ dist/server/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var port = 3030;
+var db = require('./db');
+app.use(cors());
+app.use(express.json());
+app.use('/', express.static('public'));
+app.use('/bundle', express.static('public/bundle.js'));
+app.use('/styleSheet', express.static('public/style.css'));
+app.get('/displayProduct/:id', function (req, res) {
+    var id = req.params.id;
+    db.selectOneProduct(function (err, results) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(404);
+        }
+        else {
+            res.send(results);
+        }
+    }, id);
+});
+app.patch('/updateReviewInfo', function (req, res) {
+    var id = req.body.productId;
+    var newAvg = req.body.newReviewAvg;
+    var newTotal = req.body.newReviewCount;
+    console.log(id);
+    console.log(newAvg);
+    console.log(newTotal);
+    db.updateOneProduct(function (err, results) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(404);
+        }
+        else {
+            res.send(results);
+        }
+    }, id, newAvg, newTotal);
+});
+app.listen(port, function () { return console.log("Listening on port " + port + "!"); });
+//# sourceMappingURL=server.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 const path = require('path');
-const SRC_DIR = path.join(__dirname, '/client');
-const DIST_DIR = path.join(__dirname, '/public');
+
 module.exports = {
-    entry: `${SRC_DIR}/index.js`,
-    output: {
-        filename: 'bundle.js',
-        path: DIST_DIR
-    },
-    module : {
-        rules : [
-            {
-                test : /\.js?/,
-                exclude: /node_modules/,
-                include : SRC_DIR,
-                loader : 'babel-loader',
-            }
-        ]
-    }
+  entry: path.resolve(__dirname, 'src/client/index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/client'),
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
 };
