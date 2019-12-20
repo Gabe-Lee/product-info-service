ff41aac5ac720edd0c28a98a2f443eff34987f23
Gabe-Lee <gla.ander@gmail.com>
2019-12-20 15:40:42 -0600

    Write csv creation function and seed data

--- dist/database/buildCSV.js
+++ dist/database/buildCSV.js
@@ -3,10 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var path = require("path");
 var fake = require("./fakeProduct");
-var stream = fs.createWriteStream(path.resolve(__dirname, 'seed.csv'));
+var i = 8;
+var num = i + 1;
+var start = (i * 1000000) + 1;
+var end = start + 1000000;
+var stream = fs.createWriteStream(path.resolve(__dirname, "../../../seed" + num + ".csv"));
 stream.once('open', function (fd) {
-    for (var i = 0; i < 10000000; i += 1) {
-        stream.write(fake.product() + "\n");
+    stream.write('id\tname\tminiDescription\tprice\tdealLen\tregPrice\treviewAvg\treviewCount\tbenefit\tsize\tcolor\tmattress\tlegs\tslattedBedBase\tikeaFamilySale\tonSale\tnew\tnotQuitePerfect\tavaliableForDelivery\tassembly\tsoldSeparate\n');
+    for (var j = start; j < end; j += 1) {
+        stream.write(j + "\t" + fake.product() + "\n");
     }
     stream.end();
 });
--- src/database/buildCSV.ts
+++ src/database/buildCSV.ts
@@ -2,10 +2,17 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as fake from './fakeProduct';
 
-const stream = fs.createWriteStream(path.resolve(__dirname, 'seed.csv'));
+const i = 8;
+
+
+const num = i + 1;
+const start = (i * 1000000) + 1
+const end = start + 1000000;
+const stream = fs.createWriteStream(path.resolve(__dirname, `../../../seed${num}.csv`));
 stream.once('open', (fd) => {
-  for (let i = 0; i < 10000000; i += 1) {
-    stream.write(`${fake.product()}\n`)
+  stream.write('id\tname\tminiDescription\tprice\tdealLen\tregPrice\treviewAvg\treviewCount\tbenefit\tsize\tcolor\tmattress\tlegs\tslattedBedBase\tikeaFamilySale\tonSale\tnew\tnotQuitePerfect\tavaliableForDelivery\tassembly\tsoldSeparate\n')
+  for (let j = start; j < end; j += 1) {
+    stream.write(`${j}\t${fake.product()}\n`)
   }
   stream.end();
-})
+});
